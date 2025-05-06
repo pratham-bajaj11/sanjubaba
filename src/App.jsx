1c640e833f8c3e9ba093986e297ca0fd15ee90f2
bajaj_pratham <prathambajaj0001@gmail.com>
2025-05-06 16:50:43 +0530

    dusra commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    
+      <div>
+        <h1>My react app</h1>
+        <h2>My react implemtation</h2>
+        <h3>Pratham Bajaj</h3>
+        <h1>New Updated Website </h1>
+        <h2>Updated Website</h2>
+       
+      </div>
+     
+    
+  )
+}
+
+export default App
